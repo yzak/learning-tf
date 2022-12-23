bc8fa7fe5047f28de344f9a69d5029f83eec547b
yasuo <yasuo1979@gmail.com>
2022-12-24 00:19:35 +0900

    initial

--- diagrams.py
+++ diagrams.py
@@ -6,9 +6,3 @@ with Diagram("learning-tf", show=False, direction="TB"):
     with Cluster("Region ap-northeast-1"):
         with Cluster("VPC 10.0.0.0/21"):
             InternetGateway("InternetGateway")
-            # with Cluster("AvalibitilyZone \n1c"):
-            #     with Cluster("PublicSubnet \n10.0.1.0/24"):
-            #         RouteTable("RouteTable\n1c")
-            # with Cluster("AvalibitilyZone \n1a"):
-            #     with Cluster("PublicSubnet \n10.0.0.0/24"):
-            #         RouteTable("RouteTable\n1a")
