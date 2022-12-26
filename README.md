3044d23886e05e1b628f6ec21da3c95171dfc1d8
yasuo <yasuo1979@gmail.com>
2022-12-26 22:45:41 +0900

    initial

--- README.md
+++ README.md
@@ -9,13 +9,23 @@
 
 [The for_each Meta-Argument](https://developer.hashicorp.com/terraform/language/meta-arguments/for_each)
 
+[Google Cloud 環境ディレクトリを使用する](https://cloud.google.com/docs/terraform/best-practices-for-terraform#environment-directories)
+
 ## 作るもの
 ![image](/img/learning-tf.png)
 
 ### 今回やること
 - `main.tf`
-  - PublicSubnetを作っている箇所を`foreach`を用いて管理しやすくする
-  - CIDRブロックをlocalsに定義したので、VPCのCIDRブロックも同じく定義する
+  - 開発用と本番用では、設定内容やリソースの数が異なることがあります
+  - 開発用は、サブネット２つ。本番用は、３つといったように各環境の定義を分けます
+  - `environments/dev/main.tf`にファイルを移動
+  - `environments/prd/main.tf`にファイルを移動し、サブネットを3つにする
+- `provider.tf`
+  - `environments/dev/provider.tf`にファイルを移動
+  - `environments/prd/provider.tf`にファイルを移動
+- `versions.tf`
+  - `environments/dev/versions.tf`にファイルを移動
+  - `environments/prd/versions.tf`にファイルを移動
 
 ## 1. Cloud9を起動する
 - AWSマネジメントコンソールで、cloud9と入力し、cloud9を開く
@@ -26,6 +36,7 @@
 
 ## 3. Terraformを実行する
 - 画面下部のターミナルで、コマンドを実行する
+- `cd environments/dev`
 - `terraform plan`
 - `terraform apply`
   - `yes`を入力する
@@ -33,3 +44,4 @@
 - `terraform destroy`
   - `yes`を入力する
 - AWSマネジメントコンソールで、AWSリソースが消えたことを確認する
+- `cd environments/prd`して、同じコマンドを実行
---
+++ environments/prd/main.tf
@@ -0,0 +1,99 @@
+locals {
+  vpc_cidr_block = "10.0.0.0/21"
+  public_subnets = {
+    public-1a = {
+      name = "public-1a"
+      cidr = "10.0.0.0/24"
+      az   = "ap-northeast-1a"
+    },
+    public-1c = {
+      name = "public-1c"
+      cidr = "10.0.1.0/24"
+      az   = "ap-northeast-1c"
+    },
+    public-1d = {
+      name = "public-1d"
+      cidr = "10.0.2.0/24"
+      az   = "ap-northeast-1d"
+    },
+  }
+}
+
+#----------------------------------------
+# VPCの作成
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
+#----------------------------------------
+resource "aws_vpc" "main" {
+  cidr_block           = local.vpc_cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "myproject-vpc"
+  }
+}
+
+#----------------------------------------
+# インターネットゲートウェイの作成
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway
+#----------------------------------------
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "myproject-internet-gateway"
+  }
+}
+
+#----------------------------------------
+# サブネットの作成
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+#----------------------------------------
+
+# パブリックサブネットの作成
+resource "aws_subnet" "public" {
+  for_each          = local.public_subnets
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = each.value.cidr
+  availability_zone = each.value.az
+
+  tags = {
+    Name = "myproject-${each.key}"
+  }
+}
+
+#----------------------------------------
+# ルートテーブルの作成
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
+#----------------------------------------
+resource "aws_route_table" "public" {
+  for_each = local.public_subnets
+  vpc_id   = aws_vpc.main.id
+
+  tags = {
+    Name = "myproject-${each.key}-rtb"
+  }
+}
+
+#----------------------------------------
+# IGW用のルート作成
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route
+#----------------------------------------
+resource "aws_route" "igw" {
+  for_each               = local.public_subnets
+  route_table_id         = aws_route_table.public[each.key].id
+  gateway_id             = aws_internet_gateway.main.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+#----------------------------------------
+# サブネットにルートテーブルを紐づけ
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
+#----------------------------------------
+
+# パブリックサブネットにルートテーブルを紐づけ
+resource "aws_route_table_association" "public" {
+  for_each       = local.public_subnets
+  subnet_id      = aws_subnet.public[each.key].id
+  route_table_id = aws_route_table.public[each.key].id
+}
