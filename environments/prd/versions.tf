02247346faa0e1ed5a63f08e8b4c2054f456adf2
yzak <yasuo1979@gmail.com>
2023-01-18 00:00:42 +0900

    Update versions.tf

--- environments/prd/versions.tf
+++ environments/prd/versions.tf
@@ -3,7 +3,7 @@
 # AWSプロバイダバージョンの指定
 #----------------------------------------
 terraform {
-  required_version = "1.3.6"
+  required_version = ">= 1.3.6"
 
   required_providers {
     aws = "4.45.0"
